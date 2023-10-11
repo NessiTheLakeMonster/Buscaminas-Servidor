42304512c6a43abfaeaa77fae6a72f27ab2a9ae3
Inés Mª Barrera Llerena <imbarreral020301@gmail.com>
2023-10-11 10:40:01 +0200

    Quitada la base de datos del proyecto y creada una actualizada aparte

---
+++ Proyecto_Buscaminas/Constantes.php
@@ -0,0 +1,26 @@
+<?php
+
+class Constantes
+{
+    /**
+     * Constantes usadas durante la conexión a la Base de Datos
+     */
+    static $headerMssg = "HTTP/1.1 ";
+    static $host = '127.0.0.1';
+    static $user = 'root';
+    static $psswd = '';
+    static $bdName = 'buscaminas';
+
+    /**
+     * Sentencias SQL usadas durante la conexión a la Base de Datos
+     */
+    static $insertPartida = "INSERT INTO partida VALUES(?,?,?,?,?)";
+    static $selecPartidaByID = "SELECT * FROM partida WHERE idPartida = ?";
+    static $selecPartida = "SELECT * FROM partida";
+    static $deletePartidaByID = "DELETE * FROM partida WHERE idPartida = ?";
+
+    static $selectPersona = "SELECT * FROM persona";
+    static $selectPersonaByID = "SELECT * FROM persona WHERE idUsuario = ?";
+    static $insertPersona = "INSERT INTO persona VALUES()";
+    static $deletePersonaByID = "DELETE * FROM persona WHERE idUsuario = ?";
+}
