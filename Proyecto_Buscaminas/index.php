aaca552f6e734a85f7a2112ac5cc25781e7865f7
Inés Mª Barrera Llerena <imbarreral020301@gmail.com>
2023-10-16 18:09:18 +0200

    Funcion de GET y DELETE con login funcionan correctamente, POST no

--- Proyecto_Buscaminas/index.php
+++ Proyecto_Buscaminas/index.php
@@ -13,8 +13,9 @@ $datosRecibidos = file_get_contents("php://input");
 $argus = explode('/', $paths);
 unset($argus[0]);
 
-
+// Métodos con el verbo GET
 if ($requestMethod == 'GET') {
+    // Función GET para los administradores
     if ($argus[1] == 'admin') {
         $data = json_decode($datosRecibidos, true);
 
@@ -22,9 +23,14 @@ if ($requestMethod == 'GET') {
             $data['email'],
             $data['password']
         );
-        
+
+        // Listar los usuarios en modo administrador
         if (Controlador::checkAdmin($data[0]) == true && $data[0] !== null) {
-            $data[1] = Controlador_Usuario::allUsuarios();
+            if ($argus[2] == null) {
+                $data[1] = Controlador_Usuario::allUsuarios();
+            } else {
+                $data[1] = Controlador_Usuario::usuarioByID($argus[2]);
+            }
         } else {
             $msgError = [
                 'Cod:' => 200,
@@ -33,8 +39,81 @@ if ($requestMethod == 'GET') {
 
             echo json_encode($msgError);
         }
+
+        // Función GET para los jugadores
     } else {
         // codigo para el jugador
+    }
+}
+
+// Métodos con el verbo POST
+if ($requestMethod == 'POST') {
+    // Función POST para los administradores
+    if ($argus[1] == 'admin') {
+        $data = json_decode($datosRecibidos, true);
+
+        $data[0] = Controlador::login(
+            $data['email'],
+            $data['password']
+        );
+
+        print_r($data[0]);
+        print_r(Controlador::checkAdmin($data[0]));
+
+        if (Controlador::checkAdmin($data[0]) == true && $data[0] !== null) {
+            $data[1] = Controlador_Usuario::crearUsuario(
+                Factoria::crearPersona (
+                    $data['idUsuario'],
+                    $data['password'],
+                    $data['nombre'],
+                    $data['email'],
+                    $data['partidasJugadas'],
+                    $data['partidasGanadas'],
+                    $data['admin']
+                )
+            );
+
+            print_r($data);
+        } else {
+            $msgError = [
+                'Cod:' => 200,
+                'Mensaje:' => "Usuario no es administrador"
+            ];
+
+            echo json_encode($msgError);
+        }
+    }
+}
+
+if ($requestMethod == 'DELETE') {
+
+    if ($argus[1] == 'admin') {
+        $data = json_decode($datosRecibidos, true);
+
+        $data[0] = Controlador::login(
+            $data['email'],
+            $data['password']
+        );
+
+        if (Controlador::checkAdmin($data[0]) == true && $data[0] !== null) {
+            if ($argus[2] == null) {
+                $msgError = [
+                    'Cod:' => 200,
+                    'Mensaje:' => "No has metido que usuario quieres borrar"
+                ];
+
+                echo json_encode($msgError);
+            } else {
+                $data[1] = Controlador_Usuario::borrarUsuario($argus[2]);
+            }
+        } else {
+            $msgError = [
+                'Cod:' => 200,
+                'Mensaje:' => "Usuario no es administrador"
+            ];
+
+            echo json_encode($msgError);
+        }
     }
 }
 
